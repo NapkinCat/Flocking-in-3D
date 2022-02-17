c5abeb7574a80238a356c344e618133a0de47a57
James Devlin <89040984+NapkinCat@users.noreply.github.com>
2022-02-17 12:57:42 -0700

    Addition of Menu
    Semi-functional Main Menu Scene

---
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
